02fe2560b4d80f63253b6218296407d022bb5c2f
awsdevopsmanu <83927359+awsdevopsmanu@users.noreply.github.com>
2021-05-09 21:34:58 -0400

    changes in branch online

--- pythontest.py
+++ pythontest.py
@@ -3,4 +3,5 @@ echo "hello"
 =======
 echo "hellogithub"
 echo "I have made some changes in the local python file called pythontest"
+echo "change made in branch named branch"
 >>>>>>> Stashed changes
