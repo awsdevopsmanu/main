1c73d35ac6eab837ab38097f69c1799b86490f8e
awsdevopsmanu <83927359+awsdevopsmanu@users.noreply.github.com>
2021-05-09 21:46:44 -0400

    last update on branch

--- pythontest.py
+++ pythontest.py
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
 echo "hello"
-=======
 echo "hellogithub"
 echo "I have made some changes in the local python file called pythontest"
 echo "change made in branch named branch"
->>>>>>> Stashed changes
